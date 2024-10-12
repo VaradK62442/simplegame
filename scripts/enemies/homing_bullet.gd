7743f67c253dec7a5ffe50f263de6c02e23c934a
Paul Anderson <2726368a@student.gla.ac.uk>
2024-10-12 15:56:51 +0100

    added homing bullet

--- scripts/enemies/homing_bullet.gd
+++ scripts/enemies/homing_bullet.gd
@@ -1,3 +1,14 @@
-extends enemy_basic
+extends "res://scripts/enemies/enemy_basic.gd"
+
+var target
+
+func _ready() -> void:
+	super._ready()
+	target = get_parent().get_node("Player")
+
+
 
 func _process(delta: float) -> void:
+	if target:
+		var direction = (target.position - position).normalized()
+		position += direction * 50 * delta
