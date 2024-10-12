70e91ba32f82b6b0eca659fdf301456556ad0260
Gavin Angus <gavin.angus04@gmail.com>
2024-10-12 22:00:18 +0100

    added bomb colour change

--- scripts/enemies/enemy_bomb.gd
+++ scripts/enemies/enemy_bomb.gd
@@ -1,5 +1,13 @@
 extends EnemyBasic
 
+var spawn_time = randi_range(10, 15)
+var direction = Vector2(0, 1).rotated(rotation)
+
+
+func _init() -> void:
+	spawn_chance = 20
+	speed = 25
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,5 +15,17 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var color_change_timer = 0.0
+var color_change_interval = 1.0 # Change every 1 second
+
 func _process(delta: float) -> void:
-	pass
+	position += direction * speed * delta
+	var sprite = self.get_node("Sprite2D")
+	
+	color_change_timer += delta
+	if color_change_timer >= color_change_interval:
+		color_change_timer = 0.0
+		if randi() % 2 == 0:
+			sprite.modulate = Color(0, 1, 0) # Green
+		else:
+			sprite.modulate = Color(1, 0, 0) # Red
