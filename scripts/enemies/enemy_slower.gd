3003f69522b60686636d03941bc9f8c9a4a79ce2
Paul Anderson <paulrtanderson@gmail.com>
2024-10-12 21:29:40 +0100

    added slower

---
+++ scripts/enemies/enemy_slower.gd
@@ -0,0 +1,20 @@
+extends EnemyBasic
+
+var target
+var spawntime
+var direction = Vector2.ZERO
+
+
+func _init() -> void:
+	spawn_chance = 70
+	speed = 200
+
+# Called when the node enters the scene tree for the first time.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _on_body_entered(body: Node2D) -> void:
+	# tell only player
+	if body.name == "Player":
+		body.slow_down(self)
+		queue_free()
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 400
-
+var slowdown = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -20,7 +20,7 @@ func process_input(delta: float):
 		vel.y -= 1
 
 	if vel.length() > 0:
-		vel = vel.normalized() * speed
+		vel = vel.normalized() * speed * slowdown
 	
 	move_and_collide(vel * delta)
 
@@ -29,6 +29,15 @@ func take_damage(bullet):
 	queue_free()
 	# TODO: add game over
 
+func slow_down(bullet):
+	var slowdown_duration = 5
+	$slowdowntimer.start(slowdown_duration)
+	slowdown = 0.5
+	pass
+
+func _on_slowdowntimer_timeout():
+	print("fast again")
+	slowdown = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
