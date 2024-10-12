f6b2e3eb3d3794cd88c7f116ac3d429c654c54c0
Paul Anderson <2726368a@student.gla.ac.uk>
2024-10-12 16:56:04 +0100

    10 second homing limit

--- scripts/enemies/homing_bullet.gd
+++ scripts/enemies/homing_bullet.gd
@@ -1,14 +1,18 @@
 extends "res://scripts/enemies/enemy_basic.gd"
 
 var target
-
+var spawntime
+var direction
 func _ready() -> void:
 	super._ready()
+	spawntime = 0
 	target = get_parent().get_node("Player")
 
 
 
 func _process(delta: float) -> void:
-	if target:
-		var direction = (target.position - position).normalized()
-		position += direction * 50 * delta
+	spawntime += delta
+	if target and spawntime < 10:
+		direction = (target.position - position).normalized()
+		rotation = direction.angle()
+	position += direction * 50 * delta
