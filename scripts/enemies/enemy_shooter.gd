65fb8eceadb0f422ee2cd64b3753136d38912742
k9956245 <89276559+k9956245@users.noreply.github.com>
2024-10-12 23:56:40 +0100

    fixed conflict

--- scripts/enemies/enemy_shooter.gd
+++ scripts/enemies/enemy_shooter.gd
@@ -9,11 +9,7 @@ var enemy = load("res://scenes/enemies/enemy_shooter_minion.tscn")
 @export var minion_spawnrate = 3 #seconds
 
 func _init() -> void:
-<<<<<<< Updated upstream
-	spawn_chance = 10
-=======
 	spawn_chance = 2
->>>>>>> Stashed changes
 	speed = 50
 
 # Called when the node enters the scene tree for the first time.
