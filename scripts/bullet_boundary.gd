31ab0c980f499bc1b3e544e9c67a96adc95d860f
Paul Anderson <2726368a@student.gla.ac.uk>
2024-10-12 18:29:19 +0100

    kill far bullets

--- scripts/bullet_boundary.gd
+++ scripts/bullet_boundary.gd
@@ -12,5 +12,5 @@ func _process(delta: float) -> void:
 	pass	
 
 
-func _on_body_entered(body: Node2D) -> void:
-	body.queue_free()
+func _on_area_entered(area: Area2D) -> void:
+	area.queue_free() # eplace with function body.
