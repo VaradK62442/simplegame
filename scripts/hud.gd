b4177fc4d8177d14c9b613dbe0ef5278239bb3fc
“Liam <2759113M@student.gla.ac.uk>
2024-10-13 05:13:19 +0100

    red glow

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -5,6 +5,7 @@ var hearts
 var time = 0
 var up = true
 var cur = 0
+var glow
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +13,7 @@ func _ready() -> void:
 	hearts.append($Heart1)
 	hearts.append($Heart2)
 	hearts.append($Heart3)
+	glow = get_parent().get_node("Low-health-glow")
 
 func update_health(health):
 	for i in range(len(hearts)):
@@ -20,6 +22,8 @@ func update_health(health):
 			hearts[i].modulate.r = 0.2
 			hearts[i].modulate.b = 0.5
 			hearts[i].modulate.g = 0.5
+	if health == 1:
+		glow.visible = true
 			
 # Called every frame. 'delta' is the elapsed time since the previous frame
 func _process(delta: float) -> void:
