a949b944165e4d9c7eda8255643e00efb3f1930b
Paul Anderson <paulrtanderson@gmail.com>
2024-10-13 02:53:17 +0100

    slower points to player

--- scripts/enemies/enemy_slower.gd
+++ scripts/enemies/enemy_slower.gd
@@ -9,11 +9,17 @@ var duration
 func _init() -> void:
 	slow_down = 0.5
 	duration = 5
-	spawn_chance = 20
+	spawn_chance = 10
 	speed = 400
 
 # Called when the node enters the scene tree for the first time.
-
+func _ready() -> void:
+	super._ready()
+	await get_tree().create_timer(0.1).timeout
+	target = get_parent().get_node("Player")
+	direction = (target.position - position).normalized()
+	rotation = direction.angle() - PI/2
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _on_body_entered(body: Node2D) -> void:
